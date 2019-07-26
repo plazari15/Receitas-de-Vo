0fa37399705dc1dd086b9f3f51615f3a211b2a89
Pedro <pedro.lazari@fcamara.com.br>
2019-07-26 17:28:45 -0300

    Criando Seed

---
+++ database/seeds/CategorySeeder.js
@@ -0,0 +1,45 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| CategorySeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+class CategorySeeder {
+  async run () {
+
+    const data = [
+      {
+        "email" : "plazari96@gmail.com",
+        "name" : "Pedro",
+        "password" : "PEDRO",
+        "username" : "plazari"
+      },
+      {
+        "email" : "plazari96@gmail.com",
+        "name" : "Pedro",
+        "password" : "PEDRO",
+        "username" : "plazari"
+      },
+      {
+        "email" : "plazari96@gmail.com",
+        "name" : "Pedro",
+        "password" : "PEDRO",
+        "username" : "plazari"
+      }
+    ];
+
+    const user = await User.create(data);
+
+  }
+}
+
+module.exports = CategorySeeder
