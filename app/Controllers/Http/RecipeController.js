db998cdbb9f93e711a12b01da6d0465d5482a913
Pedro <pedro.lazari@fcamara.com.br>
2019-08-06 10:24:39 -0300

    Diferente de Null

--- app/Controllers/Http/RecipeController.js
+++ app/Controllers/Http/RecipeController.js
@@ -161,7 +161,7 @@ class RecipeController {
       const fileName = await generatePhotoName(id, file.extname);
       const recipe = await Recipe.find(id);
 
-      if(!is_empty(recipe.photo) && await Driver.disk('s3').exists(recipe.photo)){
+      if(recipe.photo !== null && await Driver.disk('s3').exists(recipe.photo)){
         console.log('Existe')
         await Driver.disk('s3').delete(recipe.photo);
       }
