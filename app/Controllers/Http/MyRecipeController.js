4747d4dba34e252b2efc69008c1b001879c15690
Pedro <pedro.lazari@fcamara.com.br>
2019-08-13 10:21:59 -0300

    Criando COntroller

---
+++ app/Controllers/Http/MyRecipeController.js
@@ -0,0 +1,69 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with myrecipes
+ */
+class MyRecipeController {
+  /**
+   * Show a list of all myrecipes.
+   * GET myrecipes
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, auth }) {
+  }
+
+  /**
+   * Create/save a new myrecipe.
+   * POST myrecipes
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single myrecipe.
+   * GET myrecipes/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update myrecipe details.
+   * PUT or PATCH myrecipes/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a myrecipe with id.
+   * DELETE myrecipes/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = MyRecipeController
